c1c7b7f99e272adec3cf63375ce66e0c065f1366
Osorabrian <osorabrian@gmail.com>
2023-03-24 05:31:45 +0300

    completed assignment

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+
+    def create
+        user = User.find_by(username: params[:username])
+        if user&.authenticate(params[:password])
+            session[:user_id] = user.id
+            render json: user, status: :created
+        else
+            render json: {errors: "Not authorized"}, status: :unauthorized
+        end
+    end
+
+    def destroy
+        session.delete(:user_id)
+        head :no_content
+    end
+
+    private
+
+    def user_params
+        params.permit(:username, :password)
+    end
+end
