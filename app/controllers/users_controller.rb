f4c3ff3dbf9c021e6b95965b371a78f45c4af663
Osorabrian <osorabrian@gmail.com>
2023-03-24 05:38:55 +0300

    completed assignment

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 
-    def create_table
+    def create
         user = User.create(user_params)
         if user.valid?
             render json: user, status: :created
